ab869b7ee2c1ca714706ba3325526b46167cc90f
sagarg2000 <sagargalole.indiaactive@gmail.com>
2023-10-07 13:17:28 +0530

    Title changed

--- receipt.php
+++ receipt.php
@@ -15,7 +15,7 @@ $fetch=mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM payment Where name=
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
-    <title>Document</title>
+    <title>Receipt Github</title>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="print-styles.css" media="print">
